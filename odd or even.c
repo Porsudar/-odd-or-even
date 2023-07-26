37c40ce518c3b1ad16f4b791ea7b528a20cd9569
Porsudar Vallal <59173262+Porsudar@users.noreply.github.com>
2023-07-26 22:33:45 +0530

    Add files via upload

---
+++ odd or even.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    if(n%2==0){
+        printf("%d is a even nos",n);
+    }
+    else{
+        printf("%d is odd nos",n);
+    }
+}
